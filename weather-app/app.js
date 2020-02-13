011a331f8440fb7b8da2a9e55cce1100c2f8d183
Igor <ais_93@mail.ru>
2020-02-13 23:02:10 +0300

    forecast function

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
 const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-geocode("Moscow", (error, data) => {
+// geocode("Moscow", (error, data) => {
+//   console.log(data);
+// });
+
+forecast(-75.7088, 44.1545, (error, data) => {
   console.log(data);
 });
