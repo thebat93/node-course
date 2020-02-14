ece9cb35eb6b32729c361f5a2286d46860652bc4
Igor <ais_93@mail.ru>
2020-02-14 23:30:04 +0300

    dynamic pages with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,20 @@ const express = require("express");
 const app = express();
 const publicDirectory = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectory));
+
+app.get('', (req, res) => {
+  res.render('index', { title: 'Weather App' });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/help', (req, res) => {
+  res.render('help');
+});
 
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
