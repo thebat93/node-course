12917dd17371634e6a4dbcd2b1f2097129c4585e
Igor <ais_93@mail.ru>
2020-02-14 23:38:15 +0300

    customizing views directory

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,21 +2,28 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-const publicDirectory = path.join(__dirname, '../public');
 
-app.set('view engine', 'hbs')
+// Define paths for Express config
+const publicDirectory = path.join(__dirname, "../public");
+const viewsDirectory = path.join(__dirname, "../templates");
+
+// Setup Handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsDirectory);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectory));
 
-app.get('', (req, res) => {
-  res.render('index', { title: 'Weather App' });
+app.get("", (req, res) => {
+  res.render("index", { title: "Weather App" });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get('/help', (req, res) => {
-  res.render('help');
+app.get("/help", (req, res) => {
+  res.render("help");
 });
 
 app.listen(3000, () => {
