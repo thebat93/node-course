f0ffecbc148d3526a901234b0c9063a0ccfb3028
Igor <ais_93@mail.ru>
2020-02-14 23:57:31 +0300

    using partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,29 +1,34 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectory = path.join(__dirname, "../public");
-const viewsDirectory = path.join(__dirname, "../templates");
+const viewsDirectory = path.join(__dirname, "../templates/views");
+const partialsDirectory = path.join(__dirname, "../templates/partials");
 
 // Setup Handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsDirectory);
+hbs.registerPartials(partialsDirectory);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectory));
 
+const CREATOR = "Igor Alekseev";
+
 app.get("", (req, res) => {
-  res.render("index", { title: "Weather App" });
+  res.render("index", { title: "Weather App", creator: CREATOR });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About", creator: CREATOR });
 });
 
 app.get("/help", (req, res) => {
-  res.render("help");
+  res.render("help", { title: "Help", creator: CREATOR });
 });
 
 app.listen(3000, () => {
