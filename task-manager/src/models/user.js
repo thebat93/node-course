9bb3d7e4653dfae8aec572644d86c0bb90e6f804
Igor <ais_93@mail.ru>
2020-03-22 00:16:40 +0300

    login route

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -9,6 +9,7 @@ const userSchema = new mongoose.Schema({
     trim: true
   },
   email: {
+    unique: true,
     type: String,
     required: true,
     trim: true,
@@ -41,10 +42,27 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to log in");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to log in");
+  }
+
+  return user;
+};
+
+// Hash the plain text password before saving
 userSchema.pre("save", async function(next) {
   const user = this;
 
-  if (user.isModified('password')) {
+  if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
   }
 
