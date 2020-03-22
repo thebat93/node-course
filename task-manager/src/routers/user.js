aa3cf7238b78114b6c520bb052d89834934d18a4
Igor <ais_93@mail.ru>
2020-03-22 19:56:47 +0300

    generating jwt for user

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -6,8 +6,8 @@ router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
   try {
-    await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -17,7 +17,8 @@ router.post("/users/login", async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findByCredentials(email, password);
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (error) {
     res.status(400).send();
   }
