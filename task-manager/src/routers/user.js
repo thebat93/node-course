9a71c9f4b7c3616056e9674e6b860052ae1fcf1a
Igor <ais_93@mail.ru>
2020-03-30 00:35:17 +0300

    auth for patch and delete handlers

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -51,21 +51,7 @@ router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send();
-  }
-});
-
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password", "age"];
   const isValidOperation = updates.every(update =>
@@ -76,36 +62,25 @@ router.patch("/users/:id", async (req, res) => {
     return res.status(400).send({ error: "Invalid updates" });
   }
 
-  const _id = req.params.id;
   const fields = req.body;
 
   try {
-    const user = await User.findById(_id);
-
     updates.forEach(update => {
-      user[update] = fields[update];
+      req.user[update] = fields[update];
     });
 
-    await user.save();
+    await req.user.save();
 
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
+    res.send( req.user);
   } catch (error) {
     res.status(400).send(error);
   }
 });
 
-router.delete("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
+router.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
+    await req.user.remove();
+    res.send(req.user);
   } catch (error) {
     res.status(500).send();
   }
