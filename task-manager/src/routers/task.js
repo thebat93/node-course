2317a6b50cff3e45f8c4453ea9b80d98956ac8fc
Igor <ais_93@mail.ru>
2020-03-31 23:00:15 +0300

    filtering data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -17,9 +17,19 @@ router.post("/tasks", auth, async (req, res) => {
   }
 });
 
+// GET /tasks?completed=true|false
 router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+  
   try {
-    await req.user.populate("tasks").execPopulate();
+    await req.user.populate({
+      path: 'tasks',
+      match
+    }).execPopulate();
     res.send(req.user.tasks);
   } catch (error) {
     res.status(500).send();
