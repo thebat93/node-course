f5baedf7f609f544019f4b4587eed70ddc52565e
Igor <ais_93@mail.ru>
2020-04-02 22:15:00 +0300

    uploads validation

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -4,10 +4,6 @@ const auth = require("../middleware/auth");
 const router = new express.Router();
 const multer = require("multer");
 
-const upload = multer({
-  dest: "avatars"
-});
-
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
@@ -88,6 +84,20 @@ router.delete("/users/me", auth, async (req, res) => {
     res.send(req.user);
   } catch (error) {
     res.status(500).send();
+  }
+});
+
+const upload = multer({
+  dest: "avatars",
+  limits: {
+    fileSize: 1000000 // bytes
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image'));
+    }
+
+    cb(undefined, true);
   }
 });
 
