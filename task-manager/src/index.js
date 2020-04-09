76e28ec54333dffdcd0a54a53ac15c4da07501a3
Igor <ais_93@mail.ru>
2020-04-09 20:46:44 +0300

    refactoring index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,15 +1,7 @@
-const express = require("express");
+const app = require('./app');
 require("dotenv").config();
-require("./db/mongoose");
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
