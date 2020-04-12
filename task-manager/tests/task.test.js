84624ae0d1051f15fba77d6e821424ede1b1962a
Igor <ais_93@mail.ru>
2020-04-12 19:35:55 +0300

    additional tests for tasks

--- task-manager/tests/task.test.js
+++ task-manager/tests/task.test.js
@@ -2,8 +2,9 @@ const request = require("supertest");
 const app = require("../src/app");
 const Task = require("../src/models/task");
 const {
-  userOneId,
   userOne,
+  userTwo,
+  taskOne,
   setupDatabase,
   disconnectDatabase,
 } = require("./fixtures/db");
@@ -23,4 +24,26 @@ test("Should create task for user", async () => {
 
   expect(task).not.toBeNull();
   expect(task.completed).toBe(false);
+});
+
+test("Should fetch tasks for user", async () => {
+  const response = await request(app)
+    .get("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.length).toBe(2);
+});
+
+test("Should not delete other user's tasks", async () => {
+  await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+
+  const task = await Task.findById(taskOne._id);
+
+  expect(task).not.toBeNull();
 });
